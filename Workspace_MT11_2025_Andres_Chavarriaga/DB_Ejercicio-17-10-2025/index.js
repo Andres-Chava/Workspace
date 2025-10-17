4aec3b6af9cde75462ad74f3b4fec476eb07c968
Andrés <165319760+Andres-Chava@users.noreply.github.com>
2025-10-17 16:35:22 -0500

    Add files via upload

---
+++ Workspace_MT11_2025_Andres_Chavarriaga/DB_Ejercicio-17-10-2025/index.js
@@ -0,0 +1,13 @@
+//console.log('Ejecutando index.js');
+const express = require('express');
+const cors = require('cors');
+const estudiantesRoutes = require('./routes/estudiantes');
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/estudiantes', estudiantesRoutes);
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`)
+});
